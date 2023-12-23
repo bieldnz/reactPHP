0043862599d338237280eb9c363d2963dfbdab86
Gabriel Diniz <gabriel.dinizdesouza@hotmail.com>
2023-12-23 16:41:03 -0300

    removing some files

--- PHP/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $dbname = "marketplace";
-    $port = "3306";
-
-    $conn = new PDO("mysql:host=$host;dbname=". $dbname, $user, $pass);
-?>
